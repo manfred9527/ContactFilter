ddff349a58c7289fc8602f8e2df28e1c3b9d3d59
齐风修 <qifengxiu@gmail.com>
2020-06-23 11:59:37 +0800

    Initial Commit

---
+++ ContactFilter/ContactFilter.h
@@ -0,0 +1,19 @@
+//
+//  ContactFilter.h
+//  ContactFilter
+//
+//  Created by 齐风修 on 2020/6/23.
+//  Copyright © 2020 齐风修. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContactFilter.
+FOUNDATION_EXPORT double ContactFilterVersionNumber;
+
+//! Project version string for ContactFilter.
+FOUNDATION_EXPORT const unsigned char ContactFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactFilter/PublicHeader.h>
+
+
